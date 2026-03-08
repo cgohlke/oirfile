6e2202855debef9197836f6bc2cdb884a400f936
Christoph Gohlke <cgohlke@cgohlke.com>
2026-03-08 16:11:33 -0700

    Add oirfile/__init__.py

---
+++ oirfile/__init__.py
@@ -0,0 +1,9 @@
+# oirfile/__init__.py
+
+from .oirfile import *
+from .oirfile import __all__, __doc__, __version__
+
+# constants are repeated for documentation
+
+__version__ = __version__
+"""Oirfile version string."""
